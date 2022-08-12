693e6a82e6980097eaf3556560c649471073ecf0
JoseMarshall <hebojosemar@gmail.com>
2022-08-12 09:59:34 +0100

    refactor: use best practice on create new prismaClient

--- frontend/server/services/database/prisma-orm.ts
+++ frontend/server/services/database/prisma-orm.ts
@@ -12,22 +12,31 @@ import {
   TagToSelect,
 } from './database-types';
 
-export const prismaClient = new PrismaClient({
-  log: [
-    {
-      emit: 'event',
-      level: 'query',
-    },
-    {
-      emit: 'stdout',
-      level: 'error',
-    },
-    {
-      emit: 'stdout',
-      level: 'warn',
-    },
-  ],
-});
+declare global {
+  // eslint-disable-next-line no-var
+  var prismaClient: PrismaClient | undefined;
+}
+
+export const prismaClient =
+  global.prismaClient ||
+  new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prismaClient = prismaClient;
 
 type PrismaClientKnownRequestErrorCode = 'errorP2003';
 
