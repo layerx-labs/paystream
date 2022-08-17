faa1b4a149ea4014cfbe9ed4ee4765e4fc114dcc
JoseMarshall <hebojosemar@gmail.com>
2022-08-17 19:24:36 +0100

    feat: `EventListener` type

--- sdk/src/events/sablier.ts
+++ sdk/src/events/sablier.ts
@@ -1,12 +1,85 @@
-export interface CancelStreamEvent { returnValues: {'streamId': number;'sender': string;'recipient': string;'senderBalance': number;'recipientBalance': number;} }
-export interface CreateCompoundingStreamEvent { returnValues: {'streamId': number;'exchangeRate': number;'senderSharePercentage': number;'recipientSharePercentage': number;} }
-export interface CreateStreamEvent { returnValues: {'streamId': number;'sender': string;'recipient': string;'deposit': number;'tokenAddress': string;'startTime': number;'stopTime': number;} }
-export interface OwnershipTransferredEvent { returnValues: {'previousOwner': string;'newOwner': string;} }
-export interface PausedEvent { returnValues: {'account': string;} }
-export interface PauserAddedEvent { returnValues: {'account': string;} }
-export interface PauserRemovedEvent { returnValues: {'account': string;} }
-export interface PayInterestEvent { returnValues: {'streamId': number;'senderInterest': number;'recipientInterest': number;'sablierInterest': number;} }
-export interface TakeEarningsEvent { returnValues: {'tokenAddress': string;'amount': number;} }
-export interface UnpausedEvent { returnValues: {'account': string;} }
-export interface UpdateFeeEvent { returnValues: {'fee': number;} }
-export interface WithdrawFromStreamEvent { returnValues: {'streamId': number;'recipient': string;'amount': number;} }
+import { EventOptions, EventData } from 'web3-eth-contract';
+
+export interface CancelStreamEvent {
+  returnValues: {
+    streamId: number;
+    sender: string;
+    recipient: string;
+    senderBalance: number;
+    recipientBalance: number;
+  };
+}
+export interface CreateCompoundingStreamEvent {
+  returnValues: {
+    streamId: number;
+    exchangeRate: number;
+    senderSharePercentage: number;
+    recipientSharePercentage: number;
+  };
+}
+export interface CreateStreamEvent {
+  returnValues: {
+    streamId: number;
+    sender: string;
+    recipient: string;
+    deposit: number;
+    tokenAddress: string;
+    startTime: number;
+    stopTime: number;
+  };
+}
+export interface OwnershipTransferredEvent {
+  returnValues: { previousOwner: string; newOwner: string };
+}
+export interface PausedEvent {
+  returnValues: { account: string };
+}
+export interface PauserAddedEvent {
+  returnValues: { account: string };
+}
+export interface PauserRemovedEvent {
+  returnValues: { account: string };
+}
+export interface PayInterestEvent {
+  returnValues: {
+    streamId: number;
+    senderInterest: number;
+    recipientInterest: number;
+    sablierInterest: number;
+  };
+}
+export interface TakeEarningsEvent {
+  returnValues: { tokenAddress: string; amount: number };
+}
+export interface UnpausedEvent {
+  returnValues: { account: string };
+}
+export interface UpdateFeeEvent {
+  returnValues: { fee: number };
+}
+export interface WithdrawFromStreamEvent {
+  returnValues: { streamId: number; recipient: string; amount: number };
+}
+
+export const EventNames = {
+  CancelStream: 'CancelStream',
+  CreateCompoundingStream: 'CreateCompoundingStream',
+  CreateStream: 'CreateStream',
+  OwnershipTransferred: 'OwnershipTransferred',
+  Paused: 'Paused',
+  PauserAdded: 'PauserAdded',
+  PauserRemoved: 'PauserRemoved',
+  PayInterest: 'PayInterest',
+  TakeEarnings: 'TakeEarnings',
+  Unpaused: 'Unpaused',
+  UpdateFee: 'UpdateFee',
+  WithdrawFromStream: 'WithdrawFromStream',
+} as const;
+
+export type EventListenerCallBack = (error: Error, event: EventData) => void;
+
+export type EventListener = {
+  eventName: keyof typeof EventNames;
+  options?: EventOptions;
+  callback: EventListenerCallBack;
+};
