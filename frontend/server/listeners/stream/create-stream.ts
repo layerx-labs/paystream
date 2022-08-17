4c85209634af231830a81381f74920bfe33fb45c
JoseMarshall <hebojosemar@gmail.com>
2022-08-17 19:38:35 +0100

    feat: draft on `createStreamListener`

--- frontend/server/listeners/stream/create-stream.ts
+++ frontend/server/listeners/stream/create-stream.ts
@@ -1,4 +1,5 @@
-import { EventListenerCallBack } from 'paystream-sdk/src';
+import { EventListenerCallBack, CreateStreamEvent } from 'paystream-sdk/src';
+import { db } from '../../services/database';
 import { scribal } from '../../services/logger';
 
 export const createStreamListener: EventListenerCallBack = async (
@@ -8,6 +9,21 @@ export const createStreamListener: EventListenerCallBack = async (
   if (error) {
     scribal.e('Something went wrong on createStreamListener', error);
   } else {
+    createStreamFromEventData(
+      event.returnValues as CreateStreamEvent['returnValues']
+    ).then();
     scribal.i('Stream created', event.returnValues);
   }
 };
+
+async function createStreamFromEventData(
+  returnValues: CreateStreamEvent['returnValues']
+) {
+  return await db.stream.create({
+    data: {
+      ...returnValues,
+      startTime: new Date(returnValues.startTime),
+      stopTime: new Date(returnValues.stopTime),
+    },
+  });
+}
