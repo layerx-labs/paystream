03b19bd3051f532d28366789a21edf41b86869b4
JoseMarshall <hebojosemar@gmail.com>
2022-08-18 21:36:56 +0100

    refactor: call listenersInit

--- frontend/pages/api/graphql.ts
+++ frontend/pages/api/graphql.ts
@@ -1,15 +1,11 @@
 import { makeApolloServer, cors } from '../../server';
 import { RequestHandler } from '../../server/server-types';
 import { ApolloServer } from 'apollo-server-micro';
-import { registerEventListeners } from '../../utils/contract';
-import { makeEventListeners } from '../../server/listeners';
+import { listenersInit } from '../../server/listeners';
 
 let apolloServer: ApolloServer;
 
-/**
- * Receive the contract instance and register event listeners.
- */
-// registerEventListeners(contract, ...makeEventListeners());
+listenersInit().then().catch(console.error);
 
 const handler: RequestHandler = async (req, res) => {
   if (!apolloServer) {
