634f09f079d999833c2c4da9840fa0131c83cb54
JoseMarshall <hebojosemar@gmail.com>
2022-08-18 17:28:32 +0100

    refactor: remove mutations

--- frontend/server/resolvers/stream/stream.resolver.ts
+++ frontend/server/resolvers/stream/stream.resolver.ts
@@ -1,19 +1,11 @@
 import { list, listPageInfo } from '../common';
 import { stream } from './get-one';
-import { createStream } from './create';
-import { updateStream } from './update';
-import { deleteStream } from './delete';
 
 const resolver = {
   Query: {
     stream,
     streams: list({ model: 'stream' }),
     streamsPageInfo: listPageInfo({ model: 'stream' }),
-  },
-  Mutation: {
-    createStream,
-    deleteStream,
-    updateStream,
   },
 };
 
--- frontend/server/resolvers/withdraw-from-stream/withfraw-from-stream.resolver.ts
+++ frontend/server/resolvers/withdraw-from-stream/withfraw-from-stream.resolver.ts
@@ -1,17 +1,11 @@
 import { list, listPageInfo } from '../common';
 import { withdrawFromStream } from './get-one';
-import { createWithdrawFromStream } from './create';
-import { deleteWithdrawFromStream } from './delete';
 
 const resolver = {
   Query: {
     withdrawFromStream,
     withdrawsFromStream: list({ model: 'withdrawFromStream' }),
     withdrawsFromStreamPageInfo: listPageInfo({ model: 'withdrawFromStream' }),
-  },
-  Mutation: {
-    createWithdrawFromStream,
-    deleteWithdrawFromStream,
   },
 };
 
