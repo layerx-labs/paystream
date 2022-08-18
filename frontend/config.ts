10a863c2e5bce79e74bcda71d99f29b15a8b5f49
JoseMarshall <hebojosemar@gmail.com>
2022-08-18 21:34:39 +0100

    chore: typo

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,22 +1,26 @@
 export interface DappConfiguration {
-    chainId: number;
-    beproContracAddress: string;
-    sablierContracAddress: string;
-    
-    autonnect: boolean;
-    switchNetwork: boolean;
-    addNewortk: boolean;
-    disconnectOnSwitchAccount: boolean;
-    disconnectOnChangeNetwork: boolean;
+  chainId: number;
+  beproContractAddress: string;
+  sablierContractAddress: string;
+
+  autoconnect: boolean;
+  switchNetwork: boolean;
+  addNetwork: boolean;
+  disconnectOnSwitchAccount: boolean;
+  disconnectOnChangeNetwork: boolean;
 }
 
 export const dappConfig: DappConfiguration = {
-    chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string | "1337" ),
-    beproContracAddress: process.env.NEXT_PUBLIC_BEPRO_CONTRACT_ADDRESS as string | "0x37ebdd9B2adC5f8af3993256859c1Ea3BFE1465e",
-    sablierContracAddress: process.env.NEXT_PUBLIC_SABLIER_CONTRACT_ADDRESS as string | "0x37ebdd9B2adC5f8af3993256859c1Ea3BFE1465e",
-    autonnect: false,
-    switchNetwork: true,
-    addNewortk: true,
-    disconnectOnSwitchAccount: true,
-    disconnectOnChangeNetwork: true,
+  chainId: parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string | '1337'),
+  beproContractAddress: process.env.NEXT_PUBLIC_BEPRO_CONTRACT_ADDRESS as
+    | string
+    | '0x37ebdd9B2adC5f8af3993256859c1Ea3BFE1465e',
+  sablierContractAddress: process.env.NEXT_PUBLIC_SABLIER_CONTRACT_ADDRESS as
+    | string
+    | '0x37ebdd9B2adC5f8af3993256859c1Ea3BFE1465e',
+  autoconnect: false,
+  switchNetwork: true,
+  addNetwork: true,
+  disconnectOnSwitchAccount: true,
+  disconnectOnChangeNetwork: true,
 };
